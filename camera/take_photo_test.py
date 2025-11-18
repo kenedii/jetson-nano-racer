43cd68974d6079b871810c84672e33baa6d5a9c6
kenedii <55466055+kenedii@users.noreply.github.com>
2025-11-18 18:22:16 -0500

    Add photo capture functionality using OpenCV
    File tests the camera taking a photo using OpenCV

---
+++ camera/take_photo_test.py
@@ -0,0 +1,27 @@
+import cv2
+
+cap =  cv2.VideoCapture(1)
+
+if not cap.isOpened():
+	print("Error.")
+	exit()
+print("Camera opened. Press SPACE to take a photo, ESC to quit.")
+while True:
+	ret, frame = cap.read()
+	if not ret:
+		print("Error.")
+		break
+	cv2.imshow('Camera - Press SPACE to capture', frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# Space = take photo
+	if key == 32: #ascii for space
+		filename = "camera_1test.png"
+		cv2.imwrite(filename, frame)
+		print("Photo saved as " + filename )
+	if key == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
+
